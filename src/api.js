dfbb620ceb9c4ef67323bdde7f1cca754924a971
Rohit 1kar <139778273+Rohit-1kar@users.noreply.github.com>
2025-09-05 13:23:44 +0530

    Update api.js
    chang is imp

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASEURL || "http://localhost:8080", // Spring Boot backend
+  baseURL: import.meta.env.VITE_API_BASEURL || "https://notes-back-production-6673.up.railway.app/notes", // Spring Boot backend
 });
 
 export default api;
