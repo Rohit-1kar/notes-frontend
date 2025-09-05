31476cb4a750422b1a6c69b77e47ae5845e91b2c
Rohit 1kar <139778273+Rohit-1kar@users.noreply.github.com>
2025-09-05 13:46:45 +0530

    Update api.js
    now final change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASEURL || "https://notes-back-production-6673.up.railway.app/notes", // Spring Boot backend
+  baseURL: import.meta.env.VITE_API_BASEURL || "http://localhost:8080",
 });
 
 export default api;
